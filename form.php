1cd5a83c9c9fb14ad0ebe0c354e6eb1f18ef4562
arsheenSyed <68113441+arsheenSyed@users.noreply.github.com>
2021-03-08 20:48:50 -0800

    initial commit

---
+++ form.php
@@ -0,0 +1,271 @@
+<?php
+
+
+$server = "localhost";
+
+$username = "root";
+
+$password = "";
+
+$con = mysqli_connect($server, $username, $password);
+
+if(!$con) {
+    die("connection to this database failed due to" . mysqli_connect_error());
+}
+// echo "success";
+
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$Message = $_POST['Message'];
+
+$sql = "INSERT INTO `contactform`.`contact` (`Name`, `Email`, `Message`, `DateTime`) VALUES ('$name', '$email', '$Message', current_timestamp());";
+
+// echo $sql;
+
+
+if($con->query($sql) == true) {
+    $insert = true;
+}
+
+else {
+    echo "ERROR: $sql <br> $con->error";
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="bootstrap 4/css/bootstrap-grid.min.css">
+ <link rel="stylesheet" href="bootstrap 4/css/bootstrap.min.css">
+ 
+ <!-- custom file css -->
+ <link rel="stylesheet" href="style.css">
+
+ <!-- bootstrap icons -->
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+<!-- nav -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <!-- when i want my hamburger to be shown at sm -->
+  <div class="container">
+      <a href="index2.html" class="navbar-brand">Back2Bachpan</a>
+     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+         <span class="navbar-toggler-icon"></span>
+     </button>
+
+     <div class="collapse navbar-collapse" id="navbarCollapse">
+     <ul class="navbar-nav ml-auto">
+         
+         <li class="nav-item">
+             <a href="#explore-head-section" class="nav-link">Home</a>
+         </li>
+         <li class="nav-item">
+             <a href="#create-head-section" class="nav-link">About</a>
+         </li>
+         <li class="nav-item">
+             <a href="#share-head-section" class="nav-link">Projects</a>
+         </li>
+           <li class="nav-item">
+            <a href="#share-head-section" class="nav-link">Contact</a>
+        </li>
+     </ul>
+     </div>
+     </div>
+ </nav>
+
+<!-- nav -->
+      <!-- header -->
+      <div id="home-section">
+          <div class="dark-overlay">
+              <div class="home-inner">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-lg-12">
+                            <div class="jumbotron-fluid">
+                                <h1 class="display-4">Hello, world!</h1>
+                                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                                <hr class="my-4">
+                                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                                <p class="lead">
+                                  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                                </p>
+                              </div>
+                          </div>
+                       
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- header  -->
+
+      <!-- About Us head  -->
+    
+      <section id="explore-head-section">
+
+        <div class="container">
+            <div class="row">
+                <div class="col text-center mobile">
+                    <div class="p-5">
+                        <h1 class="display-4">About Us</h1>
+                        <p class="lead">Back2Bachpan founded in 2019 are serving the poor, depressed, middleclass and other people in general</p>
+                        <a href="#about-section" class="btn btn-outline-secondary">Find out more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+      <!-- About Us head -->
+
+
+      <!-- About Section -->
+      <div id="about-section" class="bg-light text-muted py-5">
+     
+        <div class="container">
+
+            <div class="row">
+                <!--  -->
+                <div class="col-md-6">
+                    <img src="banner2.jpg" alt="" class="img-fluid mb-3 rounded-dircle">
+                </div>
+
+                <div class="col-md-6">
+                    <h3>Who are we</h3>
+                    <br>
+                    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iste omnis officiis cum veniam quidem natus commodi velit quis pariatur, dicta consequuntur sed beatae asperiores! Numquam eveniet tempore mollitia sapiente   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos modi sed omnis eius sequi esse accusamus similique voluptate hic? Odio minus quibusdam blanditiis perspiciatis sunt vel, delectus explicabo libero accusamus?</p>
+                  </div>
+            </div>
+        </div>
+
+
+      </div>
+      <!-- our Projects -->
+<section id="create-head-section" class="bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+              <div class="p-5">
+                    <h1 class="display-4">PROJECTS</h1>
+                    <p >lorem ipsum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt tempora repellendus </p>
+                   
+                  <a href="" class="btn btn-outline-light">Find out more</a>
+            </div>
+        </div>
+    </div>
+</section>
+      <!-- our Projects -->
+
+      <!-- projects section -->
+    <!-- <div id="share-section" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"><h3></h3></div>
+            </div>
+        </div>
+    </div> -->
+      <!-- projects section -->
+
+<!-- slideshow section -->
+<!----> 
+<div id="project-section" class="bg-light text-muted py-5">
+<div class="container-fluid">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100 img-fluid imgsize" src="hero1.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 img-fluid imgsize" src="hero4.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 imgfluid imgsize" src="hero3.webp" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  </div>
+ </div>
+<!-- slideshow section -->
+
+   <!-- main footer -->
+  <footer id="main-footer" class="bg-dark">
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  <div class="py-4">
+                  <h1 class="h3">Back2bachpan</h1>
+                  <p>Copyright &copy; 2021</p>
+                  <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#contactModal">Contact Us</button>
+             </div>
+              </div>
+          </div>
+      </div>
+    </footer>
+<!-- mainfooter -->
+
+<!-- contact Modal -->
+<div class="modal fade text-dark" id="contactModal">
+<div class="modal-dialog">
+<div class="modal-content">
+<br>
+<div class="modal-header">
+<h5 class="modal-title" id="contactModalTitle">
+Contact Us
+</h5>
+
+</div>
+<div class="modal-body">
+<form action="form.php" method="post">
+<div class="form-group">
+<label for="name">Name</label>
+<input type="text" name="Name" class="form-control">
+</div>
+<div class="form-group">
+<label for="Email">Email</label>
+<input type="email" name="Email" class="form-control">
+</div>
+<div class="form-group">
+<label for="Message">Message</label>
+<input type="text" name="Message" class="form-control">
+</div>
+<div class="modal-footer">
+
+<button type="submit" class="btn btn-primary btn-block">Submit</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+<!-- contact Modal -->
+<!-- our Projects -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="bootstrap 4/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="bootstrap 4/css/bootstrap-grid.min.css">
+ <link rel="stylesheet" href="bootstrap 4/css/bootstrap.min.css">
+ 
+ <!-- custom file css -->
+ <link rel="stylesheet" href="style.css">
+
+ <!-- bootstrap icons -->
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+<!-- nav -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <!-- when i want my hamburger to be shown at sm -->
+  <div class="container">
+      <a href="index2.html" class="navbar-brand">Back2Bachpan</a>
+     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+         <span class="navbar-toggler-icon"></span>
+     </button>
+
+     <div class="collapse navbar-collapse" id="navbarCollapse">
+     <ul class="navbar-nav ml-auto">
+         
+         <li class="nav-item">
+             <a href="#explore-head-section" class="nav-link">Home</a>
+         </li>
+         <li class="nav-item">
+             <a href="#create-head-section" class="nav-link">About</a>
+         </li>
+         <li class="nav-item">
+             <a href="#share-head-section" class="nav-link">Projects</a>
+         </li>
+           <li class="nav-item">
+            <a href="#share-head-section" class="nav-link">Contact</a>
+        </li>
+     </ul>
+     </div>
+     </div>
+ </nav>
+
+<!-- nav -->
+      <!-- header -->
+      <div id="home-section">
+          <div class="dark-overlay">
+              <div class="home-inner">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-lg-12">
+                            <div class="jumbotron-fluid">
+                                <h1 class="display-4">Hello, world!</h1>
+                                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                                <hr class="my-4">
+                                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                                <p class="lead">
+                                  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                                </p>
+                              </div>
+                          </div>
+                       
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- header  -->
+
+      <!-- About Us head  -->
+    
+      <section id="explore-head-section">
+
+        <div class="container">
+            <div class="row">
+                <div class="col text-center moblie-size">
+                    <div class="p-5">
+                        <h1 class="display-4">About Us</h1>
+                        <p class="lead">Back2Bachpan founded in 2019 are serving thr vulnerable, marginalized, poor, depressed, middleclass and other people in general which are in dire need of support to minimize the sufferings </p>
+                        <a href="#about-section" class="btn btn-outline-secondary">Find out more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+      <!-- About Us head -->
+
+
+      <!-- About Section -->
+      <div id="about-section" class="bg-light text-muted py-5">
+     
+        <div class="container">
+
+            <div class="row">
+                <!--  -->
+                <div class="col-md-6">
+                    <img src="banner2.jpg" alt="" class="img-fluid mb-3 rounded-dircle">
+                </div>
+
+                <div class="col-md-6">
+                    <h3>Who are we</h3>
+                    <br>
+                    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique iste omnis officiis cum veniam quidem natus commodi velit quis pariatur, dicta consequuntur sed beatae asperiores! Numquam eveniet tempore mollitia sapiente   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos modi sed omnis eius sequi esse accusamus similique voluptate hic? Odio minus quibusdam blanditiis perspiciatis sunt vel, delectus explicabo libero accusamus?</p>
+                  </div>
+            </div>
+        </div>
+
+
+      </div>
+      <!-- our Projects -->
+<section id="create-head-section" class="bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+              <div class="p-5">
+                    <h1 class="display-4">Our Projects</h1>
+                    <p class="lead"> Eos quis sint eius culpa voluptatum blanditiis sed facilis dolor perspiciatis cumque exercitationem nam distinctio, id dicta dolorum eaque laboriosam. Veritatis, dolorum?</p>
+                  <a href="" class="btn btn-outline-light">Find out more</a>
+            </div>
+        </div>
+    </div>
+</section>
+      <!-- our Projects -->
+
+      <!-- projects section -->
+    <!-- <div id="share-section" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"><h3></h3></div>
+            </div>
+        </div>
+    </div> -->
+      <!-- projects section -->
+
+<!-- slideshow section -->
+<!----> 
+<div id="project-section" class="bg-light text-muted py-5">
+<div class="container-fluid">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100 img-fluid imgsize" src="banner3.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 img-fluid imgsize" src="banner3.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 imgfluid imgsize" src="banner3.jpg" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  </div>
+ </div>
+<!-- slideshow section -->
+
+   <!-- main footer -->
+  <footer id="main-footer" class="primary">
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  <br>
+                  <h1 class="h3">Back2bachpan</h1>
+                  <p>Copyright &copy; 2017 </p>
+                  <button class="btn btn-primary" data-toggle="modal" data-target="#contactModal">Contact Us</button>
+              </div>
+          </div>
+      </div>
+    </footer>
+<!-- mainfooter -->
+
+<!-- contact Modal -->
+<div class="modal fade text-dark" id="contactModal">
+<div class="modal-dialog">
+<div class="modal-content">
+<br>
+<div class="modal-header">
+<h5 class="modal-title" id="contactModalTitle">
+Contact Us
+</h5>
+
+</div>
+<div class="modal-body">
+<form action="form.php" method="post">
+<div class="form-group">
+<label for="name">Name</label>
+<input type="text" name="Name" class="form-control">
+</div>
+<div class="form-group">
+<label for="Email">Email</label>
+<input type="email" name="Email" class="form-control">
+</div>
+<div class="form-group">
+<label for="Message">Message</label>
+<input type="text" name="Message" class="form-control">
+</div>
+<div class="modal-footer">
+
+<button type="submit" class="btn btn-primary btn-block">Submit</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+<!-- contact Modal -->
+<!-- our Projects -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="bootstrap 4/js/bootstrap.min.js"></script>
+  </body>
+</html>
